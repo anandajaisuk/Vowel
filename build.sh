643f5cb2e4bafa2239c32216653554ff792a08d0
Xzycomputer <113665009+xzycomputer@users.noreply.github.com>
2023-05-06 18:39:16 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 pip install --upgrade pip
 pip install Flask
-pip install keras
-pip install keras-utils
 pip install Pillow
-pip install tensorflow-cpu
+pip install tensorflow-gpu==2.8.3
 pip install gunicorn
